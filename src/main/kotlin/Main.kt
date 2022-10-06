2261e319cef14e5d7b8ec16ac69f593c029bf5ab
erika lopes <caluerika@gmail.com>
2022-10-06 17:32:40 -0300

    comments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,3 +2,13 @@ fun main() {
     println("Olá mundo")
 }
 
+// comentário de uma linha
+
+/*
+* comentário de várias linhas
+* */
+
+/**
+ * comentários de várias linhas
+ * ideal para documentação de função
+ * */
