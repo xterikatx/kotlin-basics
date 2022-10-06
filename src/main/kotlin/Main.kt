dd215b45ba213f68fca1d3ff863c51c7b0202c9a
erika lopes <caluerika@gmail.com>
2022-10-06 18:22:18 -0300

    primeira atividade

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,13 +16,35 @@
 
 fun main() {
     var nome = "Erika Lopes"
+    var numero = 10
+    numero += 2
+    numero -= 1
+    numero /= 3
+    numero *= 12
     nome = "Calu da Silva"
     val idade = 22
     // não pode sobreescrever idade, pois é uma constante idade = 22
 
     println("hello world " + nome)
     println("Olá, $nome $idade")
-    println("Double Max ${Double.MAX_VALUE}")
+    println(
+        "Double Max ${Double.MAX_VALUE}"
+    )
+
+    println(
+        "Double Max ${Float.MAX_VALUE}"
+    )
+
+    println(
+        "Double Max ${Char.MAX_VALUE}"
+    )
+
+    /**
+     *
+     * Operadores aritmeticos
+     */
+
+    println("$numero")
 }
 // comentário de uma linha
 
---
+++ src/main/kotlin/atividade1.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var nomeCompleto = "erika lopes calu da silva"
+    println(nomeCompleto)
+
+    var textoVazio = ""
+    println(textoVazio)
+
+    val tamanho: Int = 40
+    println(tamanho)
+
+    var PIB_BR: Long
+    PIB_BR = 2404000000
+
+    println(PIB_BR)
+
+    var populationBrazil: Int = 211000000
+    println(populationBrazil)
+
+}
