4db19c4498bf8131388fbfb4d73e869bb8e2d9e8
erika lopes <caluerika@gmail.com>
2022-10-06 18:02:20 -0300

    interpolação

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,29 @@
-fun main() {
-    println("Olá mundo")
-}
+/**
+ * Tipos de dados
+ *
+ * Type
+ * Double
+ * Float
+ * Long
+ * Int
+ * Shot
+ * Byte
+ * Boolean
+ * String
+ * Char
+ */
 
+
+fun main() {
+    var nome = "Erika Lopes"
+    nome = "Calu da Silva"
+    val idade = 22
+    // não pode sobreescrever idade, pois é uma constante idade = 22
+
+    println("hello world " + nome)
+    println("Olá, $nome $idade")
+    println("Double Max ${Double.MAX_VALUE}")
+}
 // comentário de uma linha
 
 /*
